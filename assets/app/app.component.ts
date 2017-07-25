43bf432e72e3aec98d6db72e3ee3f26bd6671706
pashko.ostap <pashko.ostap@gmail.com>
2017-07-25 21:55:50 +0300

    updated app.component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html'
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styles: [
+    `
+  .author {
+    display: inline-block;
+    font-style: italic;
+    font-size: 12px;
+    width: 80%;}
+  .config {
+    display: inline-block;
+    text-align: right;
+    font-size: 12px;
+    width: 20%;
+  }
+  .app-comp-wrap {
+    font-size: 0;
+  }`
+  ]
 })
 export class AppComponent {
-    
+  message = {
+    content: 'Some message text',
+    author: 'Max'
+  };
 }
--- routes/app.js
+++ routes/app.js
@@ -1,28 +1,8 @@
 const express = require('express');
-const User = require('../assets/models/user');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  User.find({}, (err, doc) => {
-    if (err) {
-      return res.send(err);
-    }
-    res.render('node', { email: doc });
-  });
-});
-
-router.post('/', function(req, res, next) {
-  const { email } = req.body;
-  const user = new User({
-    firstName: 'firstName',
-    lastName: 'lastName',
-    password: 'password',
-    email
-  });
-
-  user.save();
-
-  res.redirect('/');
+  res.render('index');
 });
 
 module.exports = router;
