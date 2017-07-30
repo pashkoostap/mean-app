6891e271d16dd298588ef486230d59496b6f5ffd
pashko.ostap <pashko.ostap@gmail.com>
2017-07-30 17:47:07 +0300

    created user and messages models

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [BrowserModule],
-    bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-
-}
+export class AppModule {}
---
+++ assets/app/models/message.model.ts
@@ -0,0 +1,18 @@
+export class Message {
+  content: string;
+  username: string;
+  messageID: string;
+  userID: string;
+
+  constructor(
+    content: string,
+    username: string,
+    messageID: string,
+    userID: string
+  ) {
+    this.content = content;
+    this.username = username;
+    this.messageID = messageID;
+    this.userID = userID;
+  }
+}
