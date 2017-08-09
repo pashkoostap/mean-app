cde18f91bd57827cea47fe6255e066babb2934e7
pashko.ostap <pashko.ostap@gmail.com>
2017-08-09 22:46:30 +0300

    created POST /user/signin route

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const User = require('../assets/models/user');
@@ -26,6 +27,37 @@ router.post('/', (req, res, next) => {
       message: 'User created',
       result,
       status: 200
+    });
+  });
+});
+
+router.post('/signin', (err, res, next) => {
+  const { email, password } = req.body;
+  const arePassEqual = bcrypt.compareSync(password, user.password);
+
+  User.findOne({ email }, (error, user) => {
+    if (error) {
+      return res.json({
+        title: 'An error occurred',
+        error,
+        status: 500
+      });
+    }
+
+    if (!user || !arePassEqual) {
+      return res.json({
+        title: 'User not found',
+        status: 501
+      });
+    }
+
+    const token = jwt.sign({ user }, 'secret', { expiresIn: 7200 });
+
+    res.json({
+      status: 200,
+      message: 'User logged in',
+      userId: user._id,
+      token
     });
   });
 });
